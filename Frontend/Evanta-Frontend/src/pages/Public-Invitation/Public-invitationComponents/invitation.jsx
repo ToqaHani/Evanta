fc850345f4f3538704aa105c26410831ece917cd
toqa <tottohany@gmail.com>
2026-09-12 20:03:13 +0300

    feat: update invitation component with phone input and status handling

--- Frontend/Evanta-Frontend/src/pages/Public-Invitation/Public-invitationComponents/invitation.jsx
+++ Frontend/Evanta-Frontend/src/pages/Public-Invitation/Public-invitationComponents/invitation.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
-import photo from '../../../assets/Beige And Gold Floral Wedding Invitation.png'
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import photo from "../../../assets/Beige And Gold Floral Wedding Invitation.png";
 import { FaCircleCheck } from "react-icons/fa6";
 function Invitation() {
+  const { eventId } = useParams();
+  let [phone, setPhone] = useState("");
   // بغير حالة ال geust على حسب اللي اختاره
   let [status, setStatus] = useState("Pending");
   //   بغير بيها لون الكلمة جوا المسدج
@@ -9,10 +13,24 @@ function Invitation() {
   //   بظهر بيها و بخفي المسدج اللي هتظهر
   let [showMessage, setShowMessage] = useState(false);
   // بخليه يغبر كل اللي فوق
-  function handleClick(e) {
-    setStatusColor(e.target.style.backgroundColor);
-    setStatus(e.target.name);
-    setShowMessage(true);
+  async function handleClick(e) {
+    const selectedStatus = e.target.name;
+
+    try {
+      await axios.patch(
+        `http://localhost:3000/api/events/${eventId}/guests/status`,
+        {
+          phone,
+          status: selectedStatus,
+        },
+      );
+
+      setStatusColor(e.target.style.backgroundColor);
+      setStatus(selectedStatus);
+      setShowMessage(true);
+    } catch (error) {
+      console.log(error.response?.data);
+    }
   }
 
   return (
@@ -47,6 +65,21 @@ function Invitation() {
       )}
       <div className="content m-5 p-2 p-sm-5 d-flex flex-column justify-content-center align-items-center">
         <img src={photo} alt="invitation" className="invitation-img" />
+        <div className="d-flex flex-column justify-content-center align-items-center p-3">
+          <label htmlFor="phone">Please, Enter your phone number:</label>
+          <input
+            type="text"
+            id="phone"
+            value={phone}
+            maxLength={11}
+            onChange={(e) => {
+              const value = e.target.value;
+              if (/^\d*$/.test(value)) {
+                setPhone(value);
+              }
+            }}
+          />
+        </div>
         <div className="d-flex flex-column flex-sm-row mt-4 gap-3 justify-content-center align-items-center">
           <button
             name="Confirmed"
@@ -54,6 +87,7 @@ function Invitation() {
               backgroundColor: "var(--color-green)",
               border: "1px solid var(--color-green)",
             }}
+            disabled={!/^(010|011|012|015)\d{8}$/.test(phone)}
             onClick={handleClick}
           >
             Yes, I'll be there!
@@ -64,6 +98,7 @@ function Invitation() {
               backgroundColor: "var(--color-orange)",
               border: "1px solid var(--color-orange)",
             }}
+            disabled={!/^(010|011|012|015)\d{8}$/.test(phone)}
             onClick={handleClick}
           >
             Maybe
@@ -74,6 +109,7 @@ function Invitation() {
               backgroundColor: "var(--color-red)",
               border: "1px solid var(--color-red)",
             }}
+            disabled={!/^(010|011|012|015)\d{8}$/.test(phone)}
             onClick={handleClick}
           >
             Sorry, I can't.
