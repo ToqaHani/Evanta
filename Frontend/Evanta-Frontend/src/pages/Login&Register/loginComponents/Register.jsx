38874c251e4bf33fc52827228a0b25f58c210ddc
toqa <tottohany@gmail.com>
2026-09-15 11:31:48 +0300

    Refactor import statement in Register component to use Auth.css

--- Frontend/Evanta-Frontend/src/pages/Login&Register/loginComponents/Register.jsx
+++ Frontend/Evanta-Frontend/src/pages/Login&Register/loginComponents/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../../../assets/logo.png";
-import "../Login&Register.css";
+import "../Auth.css";
 
 export default function Register() {
   const navigate = useNavigate();
